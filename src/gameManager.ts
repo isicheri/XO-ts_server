d649ec0ae165d4a6ad3a4a855071b49e1a86cd36
Isicheri <tech2dom@gmail.com>
2025-01-27 00:31:38 +0100

    made some changes to the server

--- src/gameManager.ts
+++ src/gameManager.ts
@@ -9,7 +9,7 @@ export class GameManager {
 
     constructor() {
      this.board = [
-        [' ',' ' ,' '],
+        [' ',' ',' '],
         [' ',' ',' '],
         [' ',' ',' ']
     ];
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express,{ Express } from "express";
+import express,{ Express, Request, Response } from "express";
 import cors from "cors";
 import http from "http";
+import uuid from "uuidv4";
 import { Server,Socket } from "socket.io";
 import { GameManager } from "./gameManager";
 
@@ -12,11 +13,29 @@ const io = new Server(server,{
     }
 });
 app.use(cors({
-    origin:["http://localhost:5173/"]
+    origin:["http://localhost:5173/"],
+    allowedHeaders: []
 }))
+app.use(express.json())
 const gameManager = new GameManager();
+let rooms: {[roomId: string]: {creatorId: string,creator: string; players: string[]; spectators: string[]; gameState: {
+    board: string[][],
+    currentPlayer: number
+}}} = {};
 
-let rooms: {[roomId: string]: {creator: string; players: string[] | null; spectators: string[]; gameState: {}}} = {};
+app.get("/api_v1/:roomId", (req:Request,res:Response) => {
+  const {roomId} = req.params;
+  const room = rooms[roomId]
+  if(room) {
+    res.json({
+        exist: true
+    })
+  }else {
+    res.json({
+        exist: false
+    })
+  }
+})
 
 io.on('connecton',(socket:Socket) => {
 console.log("a user connected:",socket.id);
@@ -24,6 +43,7 @@ console.log("a user connected:",socket.id);
 socket.on("create_room", (creatorName: string) => {
     const roomId = `room_${Math.random().toString(36).substring(2,9)}`;
     rooms[roomId] = {
+        creatorId: socket.id,
         creator: creatorName,
         players: gameManager.players,
         spectators: gameManager.spectators,
@@ -39,75 +59,48 @@ socket.on("create_room", (creatorName: string) => {
     console.log("Room:",roomId,"by",creatorName);
 })
 
-//start game
-socket.on("start_game",(roomId: string,creatorName:string) => {
-    //Add a player if there is space
-// if(gameManager.players.length < 2) {
-//     gameManager.players.push(socket.id);
-//     socket.emit("waiting","Waiting for player");
-//     if(gameManager.players.length === 2) {
-//      //start the game when both players are connected
-//      io.to(gameManager.players[0]).emit('start_game',{player: 'X'})
-//      io.to(gameManager.players[1]).emit('start_game',{player: 'O'})
-//      //notify spectators that the game has started
-//      io.emit("game_started","Game has started!")
-//     }
-// }else if(gameManager.players.length === 2 && gameManager.spectators.length < 15) {
-//     gameManager.spectators.push(socket.id)
-//     socket.emit("spectator_joined","You are now a spectator");
-//     socket.emit("game_started","Game is already in progress!");
-//     //send the current board to the spectators
-//     socket.emit('update_board',{ board:gameManager.board });
-// }else {
-//     socket.emit("game_full","Game is full. Please wait.");
-// }
-})
-
-socket.on("join_game_as_player",(roomId: string,playerName:string) => {
-    const room = rooms[roomId];
+socket.on("join_room",(data: {roomId: string, userName: string, role: "player" | "spectator"}) => {
+    const room = rooms[data.roomId];
     if(!room) {
         socket.emit('error', "Room Does Not Exist");
         return;
+        // io.to(gameManager.players[0]).emit('start_game',{player: 'X'})
+//      io.to(gameManager.players[1]).emit('start_game',{player: 'O'})
     }
-    if(room.players?.length == 2) {
-        socket.emit("error","players cannot be more that two");
-        return;
+    if(data.role === "player") {
+        if(room.players?.length == 2) {
+                socket.emit('error', "players cannot be more than two,join as spectator");
+                return;
+        }else {
+            console.log(socket.id)
+            room.players?.push(data.userName)
+            io.to(data.roomId).emit("player_joined",{username: data.userName}) 
+        }
+    }else if(data.role === "spectator") {
+        if(room.spectators.includes(data.userName)) {
+            socket.emit("error","Already a spectator");
+            return;
+        }
+        if(room.spectators.length === 20) {
+            socket.emit("error","room is already full");
+        }
+        room.spectators.push(data.userName)
+        socket.join(data.roomId);
+        io.to(data.roomId).emit("spectator_joined",{userName: data.userName})
+        socket.emit("game_started","Game is already in progress!");
+    }else {
+    socket.emit("error","role does not exists");
     }
-    room.players?.push(playerName);
-    io.to(roomId).emit("player_invited",playerName);
-    console.log("player name:", playerName);
-})
-
-
-socket.on("join_as_spectator",(roomId:string,spectatoreName: string) => {
-    const room = rooms[roomId];
-    if(!room) {
-        socket.emit('error',"Room does not exist");
-        return;
-    }
-    if(room.spectators.includes(spectatoreName)) {
-        socket.emit("error","Already a spectator");
-        return;
-    }
-    if(room.spectators.length === 20) {
-        socket.emit("error","room is already full");
-        return; 
-    }
-    room.spectators.push(spectatoreName);
-    socket.join(roomId);
-    io.to(roomId).emit("spectator_joined",spectatoreName);
-    socket.emit("game_started","Game is already in progress!");
-    console.log("Specator",spectatoreName,"joined the room",roomId);
 })
 
 
 socket.on("make_move",(data: {row: number,col: number,roomId: string}) => {
     const {row,col,roomId} = data;
     const room = rooms[roomId]
-    if(gameManager.board[row][col] === ' ') {
-        const symbol = gameManager.currentPlayer === 0 ? 'X' : 'O';
-        gameManager.board[row][col] = symbol;
-        gameManager.currentPlayer = (gameManager.currentPlayer + 1) % 2;
+    if(room.gameState.board[row][col] === ' ') {
+        const symbol = room.gameState.currentPlayer === 0 ? 'X' : 'O';
+        room.gameState.board[row][col] = symbol;
+        room.gameState.currentPlayer = (gameManager.currentPlayer + 1) % 2;
         const winner = gameManager.checkWinner();
         if(winner) {
      io.to(gameManager.players[0]).emit('game_over',`${winner} wins!`);
@@ -119,9 +112,9 @@ socket.on("make_move",(data: {row: number,col: number,roomId: string}) => {
         [' ',' ',' ']
     ];
         }
-    }else if(gameManager.board.flat().every((cell) => cell !== ' ')) {
-        io.to(gameManager.players[0]).emit('game_over',"It's a draw");
-        io.to(gameManager.players[1]).emit('game_over',"It's a draw");
+    }else if(room.gameState.board.flat().every((cell) => cell !== ' ')) {
+        io.to(room.players[0]).emit('game_over',"It's a draw");
+        io.to(room.players[1]).emit('game_over',"It's a draw");
         io.emit('game_over',"It's a draw");
         gameManager.board =  [
            [' ',' ' ,' '],
@@ -129,9 +122,9 @@ socket.on("make_move",(data: {row: number,col: number,roomId: string}) => {
            [' ',' ',' ']
         ]
     }
-    io.to(gameManager.players[0]).emit('update_board', {board: gameManager.board})
-    io.to(gameManager.players[1]).emit('update_board', {board: gameManager.board})
-    io.emit("update_board",{board:gameManager.board})
+    io.to(room.players[0]).emit('update_board', {board: room.gameState.board})
+    io.to(room.players[1]).emit('update_board', {board: room.gameState.board})
+    io.emit("update_board",{board:room.gameState.board})
 })
 
 //handle player disconnecting 
